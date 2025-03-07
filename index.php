dd6ffd4440b48252ce665e2dc7ee582078a12c50
Anoulakdev <105956871+Anoulakdev@users.noreply.github.com>
2025-03-07 14:45:12 +0700

    07032025

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>MEM VOTE</title>
+    <title>ກອງ​ປະ​ຊຸ​ມ​ພັກຮາ​ກ​ຖານ ຟ​ຟ​ລ</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
